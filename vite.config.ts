8a32bd2d524e8d09c3d1a4d3b047d652b36bdc4f
Yakirbe <yaqir1@gmail.com>
2024-11-23 15:34:45 +0200

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chilly-demo/', // Ensure this matches your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'docs', // Output to 'docs' directory
   },
 });
